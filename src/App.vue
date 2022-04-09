2f202f87cb1193e928d5d497939c5f5df923de5d
dxide <1578567673@qq.com>
2022-04-09 22:00:04 +0800

    4月9日 4-6 首页布局开发

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-
-  <router-view></router-view>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 <script>
 
 </script>
 <style>
-
+/*全局样式*/
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app{
+  height: 100vh;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,50 @@
 <template>
-<h1>hello</h1>
-  <el-button>hello</el-button>
+  <el-container
+      style="height: 100%; border: 1px solid #eee">
+    <el-header style="text-align: right; font-size: 12px">
+      <div class="toolbar">
+        <el-dropdown>
+          <el-icon style="margin-right: 8px; margin-top: 1px"
+          ><setting
+          /></el-icon>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>View</el-dropdown-item>
+              <el-dropdown-item>Add</el-dropdown-item>
+              <el-dropdown-item>Delete</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+        <span>Tom</span>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-scrollbar>
+          <el-menu :default-openeds="['1', '3']">
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><message /></el-icon>Navigator One
+              </template>
+            </el-sub-menu>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
+      <el-main>
+        main
+      </el-main>
+    </el-container>
+    <el-footer >
+      <div style="text-align: center;margin-top: 20px;">
+        Wiki电子书 @2022 Create By xulong
+      </div>
+    </el-footer>
+  </el-container>
 </template>
 
-<script>
-export default {
-  name: "Home"
-}
+
+<script lang="ts" setup>
+
 </script>
 
 <style scoped>
