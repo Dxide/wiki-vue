acfba494ffedd6d721e45ffa26a54dc10cfc5cd9
dxide <1578567673@qq.com>
2022-04-10 22:40:12 +0800

    4月10日  主页显示电子书数据

--- src/views/Ebook.vue
+++ src/views/Ebook.vue
@@ -11,8 +11,18 @@
     </el-aside>
 
     <el-main>
-      <pre>{{ ebooks }}</pre>
-
+      <el-row :gutter="30">
+        <el-col :span="8" v-for="ebook in ebooks" style="margin-bottom: 30px">
+          <el-card>
+            <template #header>
+              <div>
+                <span>{{ ebook.name}}</span>
+              </div>
+            </template>
+            <div>{{ebook.description}}</div>
+          </el-card>
+        </el-col>
+      </el-row>
     </el-main>
   </el-container>
 </template>
@@ -24,7 +34,7 @@ import {onMounted, ref} from "vue";
 const ebooks=ref()//电子书列表
 
 onMounted(()=>{
-  request.get("ebook",{params:{search:"spring"}}).then((response)=>{
+  request.get("ebook").then((response)=>{
     ebooks.value=response.data
   })
 })
