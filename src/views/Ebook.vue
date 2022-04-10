3c833fd4a405bb3e7f6a287cf3b2d4b068f5f273
dxide <1578567673@qq.com>
2022-04-10 14:58:38 +0800

    4月10日 4-7 拆分Home组件

---
+++ src/views/Ebook.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container>
+    <el-aside width="200px" style="border-right: 1px solid lightgray">
+      <el-scrollbar>
+        <el-menu>
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon>
+                <message/>
+              </el-icon>
+              Navigator One
+            </template>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+    <el-main>
+      main1223
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,7 @@
   <el-container
       style="height: 100%;" direction="vertical">
     <the-header/>
-    <el-container>
-      <el-aside width="200px" style="border-right: 1px solid lightgray">
-        <el-scrollbar>
-          <el-menu>
-            <el-sub-menu index="1">
-              <template #title>
-                <el-icon>
-                  <message/>
-                </el-icon>
-                Navigator One
-              </template>
-            </el-sub-menu>
-          </el-menu>
-        </el-scrollbar>
-      </el-aside>
-      <el-main>
-        main12
-      </el-main>
-    </el-container>
+    <Ebook/>
     <the-footer/>
   </el-container>
 </template>
@@ -29,6 +11,8 @@
 <script lang="ts" setup>
 import TheHeader from "@/components/the-header.vue";
 import TheFooter from "@/components/the-footer.vue";
+import Ebook from "@/views/Ebook.vue";
+
 </script>
 
 <style scoped>
