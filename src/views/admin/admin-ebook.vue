156a99c448036725d22a72ec895c4848085b316c
dxide <1578567673@qq.com>
2022-04-13 15:48:26 +0800

    4月13日 6-7 对话框显示电子书表单

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -13,7 +13,7 @@
       <el-table-column prop="voteCount" label="点赞数" />
       <el-table-column label="操作">
         <template #default="scope">
-          <el-button  @click="editUser(scope.row)">编辑</el-button>
+          <el-button  @click="clickEdit(scope.row)">编辑</el-button>
           <el-popconfirm
               confirm-button-text="确定"
               cancel-button-text="取消"
@@ -28,6 +28,7 @@
         </template>
       </el-table-column>
     </el-table>
+
     <el-pagination
         v-model:currentPage="pageInfo.current"
         v-model:page-size="pageInfo.size"
@@ -38,6 +39,29 @@
         @size-change="getEbooks"
         style="margin-top: 10px;float: right"
     />
+
+    <el-dialog v-model="dialogEditVisible" title="编辑" width="40%">
+      <el-form :model="ebookForm">
+        <el-form-item label="封面" >
+          <el-input v-model="ebookForm.cover" clearable/>
+        </el-form-item>
+        <el-form-item label="名称" >
+          <el-input v-model="ebookForm.name" clearable/>
+        </el-form-item>
+        <el-form-item label="分类" >
+          <el-input v-model="ebookForm.categoryId" />
+        </el-form-item>
+        <el-form-item label="描述" >
+          <el-input v-model="ebookForm.description" type="textarea" clearable/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogEditVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogEditVisible = false">提交</el-button>
+      </span>
+      </template>
+    </el-dialog>
   </el-main>
 </template>
 
@@ -48,13 +72,21 @@ import {InfoFilled,Delete,Edit} from '@element-plus/icons-vue'
 
 const ebooks=ref()//电子书列表
 const tableLoading=ref(true)//表格加载
-const pageInfo=ref({//分页数据
+const submitLoading=ref(false)//提交加载框
+const dialogEditVisible=ref(false)//编辑页面对话框
+const ebookForm=ref({
+  cover:'',
+  name:'',
+  categoryId:'',
+  description:''
+})//电子书表单
+const pageInfo=ref({
   total: 0,
   size: 5,
   current: 1
-})
+})//分页数据
 
-const getEbooks=()=>{//获取电子书列表
+const getEbooks=()=>{
   tableLoading.value=true
   request.get("ebook",{params:{currentPage:pageInfo.value.current
       ,pageSize:pageInfo.value.size}}).then((response)=>{
@@ -62,11 +94,15 @@ const getEbooks=()=>{//获取电子书列表
       ebooks.value=response.data.records
       tableLoading.value=false
   })
-}
+}//获取电子书列表
+const clickEdit=(row:object)=>{
+  dialogEditVisible.value=true
+  ebookForm.value=JSON.parse(JSON.stringify(row))
+}//编辑按钮点击事件
 
 onMounted(()=>{
   getEbooks()
-})
+})//初始化
 </script>
 
 <style scoped>
