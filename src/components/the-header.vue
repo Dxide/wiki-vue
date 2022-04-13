8ad879d40f2a44b43f2b99704326b602e07120f3
dxide <1578567673@qq.com>
2022-04-13 12:52:42 +0800

    4月12日 头部布局颜色优化

--- src/components/the-header.vue
+++ src/components/the-header.vue
@@ -1,20 +1,21 @@
 <template>
-  <el-header style="width: 100%;border-bottom: 1px solid lightgray">
+  <el-header style="width: 100%;border-bottom: 1px solid #dcdfe6">
     <el-row>
       <el-col :span="6" style="margin: 20px auto;text-align: left">
         Wiki电子书
       </el-col>
       <el-col :span="12">
         <el-menu
+            :default-active='currentPath'
             class="el-menu-demo"
             mode="horizontal"
             :router=true
             :ellipsis=false
             style="width: 274px;margin: 1px auto"
         >
-          <el-menu-item index="/ebook">首页</el-menu-item>
-          <el-menu-item index="/admin/ebook">电子书管理</el-menu-item>
-          <el-menu-item index="/about">关于我们</el-menu-item>
+          <el-menu-item index="/ebook" class="item">首页</el-menu-item>
+          <el-menu-item index="/admin/ebook" class="item">电子书管理</el-menu-item>
+          <el-menu-item index="/about" class="item">关于我们</el-menu-item>
         </el-menu>
       </el-col>
       <el-col :span="6" style="margin: 20px auto;text-align: right">
@@ -26,11 +27,15 @@
 </template>
 
 <script lang="ts" setup>
+import router from "@/router";
+import {ref} from "vue";
+
+const currentPath=ref(router.currentRoute.value.path)
 
 </script>
 
 <style scoped>
 .item{
-  font-size: 20px;
+  font-size: 18px;
 }
 </style> 
