fb886c34cf64e42f5e5322eeaa64ef47b3594cfb
dxide <1578567673@qq.com>
2022-04-20 12:21:57 +0800

    4月20日 doc的增删

--- src/util/Tools.ts
+++ src/util/Tools.ts
@@ -1,16 +1,33 @@
 export class Tools{
 
     public static toTree(array:any,parentId:string){
+        if (Tools.isEmpty(array)) {
+            return [];
+        }
         const result=[]
         for (let i=0;i<array.length;i++){
             const c=array[i]
             if (c.parent===parentId){
                 result.push(c)
                 //递归获取子节点
-                c.children=Tools.toTree(array, c.id)
+                const children = Tools.toTree(array, c.id);
+                if (!Tools.isEmpty(children)) {
+                    c.children = children;
+                }
             }
         }
         return result
     }//将数组转换为树形结构
 
+    public static isEmpty (obj: any) {
+        if ((typeof obj === 'string')) {
+            return !obj || obj.replace(/\s+/g, "") === ""
+        } else {
+            return (!obj || JSON.stringify(obj) === "{}" || obj.length === 0);
+        }
+    }//空校验 null或""都返回true
+
+    public static copy(obj:any){
+        return JSON.parse(JSON.stringify(obj))
+    }//对象的复制
 }
--- src/views/admin/AdminDoc.vue
+++ src/views/admin/AdminDoc.vue
@@ -3,9 +3,10 @@
     <el-aside width="200px" style="border-right: 1px solid lightgray">
       <div style="margin-top: 10px;margin-bottom: 10px;text-align: center">
         <el-button type="primary" @click="getDoc">刷新</el-button>
-        <el-button type="primary" >新增</el-button>
+        <el-button type="primary" @click="add">新增</el-button>
       </div>
-      <el-tree :data="docs" :props="defaultProps" @node-click="handleNodeClick" :highlight-current="true" />
+      <el-tree :data="docs" :props="defaultProps" @node-click="handleNodeClick" :highlight-current="true"
+               :default-expand-all="true"/>
     </el-aside>
     <el-main>
       <el-form :model="docForm" :inline="true">
@@ -13,13 +14,16 @@
           <el-input v-model="docForm.name"></el-input>
         </el-form-item>
         <el-form-item label="父文档">
-          <el-tree-select v-model="docForm.parent" :data="treeSelect" :props="defaultProps" check-strictly />
+          <el-tree-select v-if="isReloadData" v-model="docForm.parent" :data="treeSelect" :props="defaultProps" check-strictly
+                          :default-expand-all="true"/>
         </el-form-item>
         <el-form-item label="排序">
           <el-input-number v-model="docForm.sort"></el-input-number>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submit">提交</el-button>
+          <el-button type="primary" @click="submit" v-show="booleanSubmit">提交</el-button>
+          <el-button type="primary" @click="submit" v-show="!booleanSubmit">保存</el-button>
+          <el-button type="danger" @click="deleteDoc(docForm.id)" v-show="!booleanSubmit">删除</el-button>
         </el-form-item>
       </el-form>
     </el-main>
@@ -27,7 +31,7 @@
 </template>
 
 <script lang="ts" setup>
-import {onMounted, ref} from "vue";
+import {nextTick, onMounted, ref} from "vue";
 import request from "@/util/request";
 import {Tools} from "@/util/Tools";
 import {useRoute} from "vue-router";
@@ -37,6 +41,7 @@ const theRoot=[{
   id:"0",
   parent:"0",
   name:"无",
+  children: []
 }]//树根
 const treeSelect=ref()//树形选择器的数据
 const docs=ref()//文档列表
@@ -44,20 +49,30 @@ const defaultProps = {
   children: 'children',
   label: 'name',
   value:'id',
-  emitPath:false
+  disabled:'disabled',
+  emitPath:false,
 }//默认配置
 const ebookId=useRoute().params.ebookId//ebookId
-const docForm=ref({ebookId:ebookId,sort:0})//doc表单
+const docForm=ref({ebookId:ebookId,sort:0,id:''})//doc表单
+const booleanSubmit=ref(true)//当前模式，true新增，false修改
+const isReloadData=ref(true)//局部刷新组件
 
 const getDoc = () => {
   request.get("doc?ebookId="+ebookId).then((response)=>{
     docs.value=response.data
     docs.value=Tools.toTree(docs.value,"0")//转换为树形结构
-    treeSelect.value=theRoot.concat(docs.value)//添加无选项
+    treeSelect.value=Tools.copy(theRoot).concat(Tools.copy(docs.value))//添加无选项
+    if (!Tools.isEmpty(docForm.value.id)){
+      setDisable(treeSelect.value,docForm.value.id)
+      reloadData()
+    }
   })
 }//获取文档列表
 const handleNodeClick = (data: any) => {
-  console.log(data.id)
+  booleanSubmit.value=false
+  docForm.value=Tools.copy(data)
+  treeSelect.value=theRoot.concat(Tools.copy(docs.value))//重置treeSelect数据
+  setDisable(treeSelect.value,data.id)
 }//节点点击事件
 const submit=()=>{
   request.post("doc/admin",docForm.value).then((response:any)=>{
@@ -65,6 +80,41 @@ const submit=()=>{
     getDoc()
   })
 }//提交
+const deleteDoc=(id:string)=>{
+  console.log(id)
+}//删除文档
+const setDisable=(treeSelectData:any,id:string)=>{
+  //遍历某一层节点
+  for (let i=0;i<treeSelectData.length;i++){
+    const node=treeSelectData[i]
+    if (node.id==id){//发现当前节点与与传入节点一样，禁用此选项
+      node.disabled=true
+      const children=node.children
+      if (!Tools.isEmpty(children)){
+        for (let j=0;j<children.length;j++){//将其子节点禁用
+          setDisable(children,children[j].id)
+        }
+      }
+      break
+    }else {
+      const children=node.children
+      if (!Tools.isEmpty(children)){
+          setDisable(children,id)
+      }
+    }
+  }
+}//设置不可用选项
+const add=()=>{
+  booleanSubmit.value=true
+  docForm.value={ebookId:ebookId,sort:0,id:''}
+  treeSelect.value=Tools.copy(theRoot).concat(Tools.copy(docs.value))//添加无选项
+}//添加按钮点击事件
+const reloadData = () => {
+  isReloadData.value=false
+  nextTick(()=>{
+    isReloadData.value=true
+  })
+}//局部刷新组件
 
 onMounted(()=>{
   getDoc()
